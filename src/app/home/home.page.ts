43edbbb2c5278d3561b3ee4135a642751b2b8c10
Kamlesh Kumar <kamlesh.kumar@agreeya.com>
2019-06-13 18:17:29 +0530

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  displayedColumns: string[] = ['first_name', 'last_name', 'twitter'];
+  dataSource = new MatTableDataSource<any>([
+    {
+      first_name: 'Max',
+      last_name: 'Lynch',
+      twitter: 'maxlynch'
+    },
+    {
+      first_name: 'Matt',
+      last_name: 'Netkow',
+      twitter: 'dotNetkow'
+    },
+    {
+      first_name: 'Ben',
+      last_name: 'Sperry',
+      twitter: 'benjsperry'
+    }
+  ]);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  userForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      address: ['', Validators.required]
+    });
+  }
+}
